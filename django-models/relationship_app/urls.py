8e49b55c03a5843c34dbe0648a917b127bf69e95
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-07 00:16:01 +0300

    add file

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -5,6 +5,9 @@ from .views import (
 )
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
+from relationship_app.admin_view import admin_view
+from relationship_app.librarian_view import librarian_view
+from relationship_app.member_view import member_view
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
@@ -12,7 +15,7 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("register/", views.register, name="register"),
-    path("admin/", views.admin_view, name="admin_view"),
-    path("librarian/", views.librarian_view, name="librarian_view"),
-    path("member/", views.member_view, name="member_view"),
+    path("admin/", admin_view, name="admin_view"),
+    path("librarian/", librarian_view, name="librarian_view"),
+    path("member/", member_view, name="member_view"),
 ]
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,8 +7,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth import login
-from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponseForbidden
+
 
 # Create your views here.
 
@@ -43,28 +42,4 @@ def register(request):
     return render(request, "register.html", {"form": form})
 
 
-def is_admin(user):
-    return user.userprofile.role == "Admin"
 
-
-@user_passes_test(is_admin)
-def admin_view(request):
-    return render(request, "admin_view.html")
-
-
-def is_librarian(user):
-    return user.userprofile.role == "Librarian"
-
-
-@user_passes_test(is_librarian)
-def librarian_view(request):
-    return render(request, "librarian_view.html")
-
-
-def is_member(user):
-    return user.userprofile.role == "Member"
-
-
-@user_passes_test(is_member)
-def member_view(request):
-    return render(request, "member_view.html")
