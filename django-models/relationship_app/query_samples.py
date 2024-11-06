ff655007a57d754af71314195600d539c0541ad9
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-06 23:00:22 +0300

    add file

--- django-models/relationship_app/query_samples.py
+++ django-models/relationship_app/query_samples.py
@@ -3,7 +3,7 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 def get_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
+    books = Book.objects.filter(author=author)
     return [book.title for book in books]
 
 
