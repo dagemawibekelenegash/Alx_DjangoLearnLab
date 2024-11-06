0c58046054c1a0b6964ee3051d17337c3c03c229
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-06 23:34:53 +0300

    add file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.views import LoginView, LogoutView
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
 
 # Create your views here.
 
@@ -15,3 +19,22 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = "relationship_app/library_detail.html"
     context_object_name = "library"
+
+
+class CustomLoginView(LoginView):
+    template_name = "login.html"
+
+
+class CustomLogoutView(LogoutView):
+    template_name = "logout.html"
+
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
