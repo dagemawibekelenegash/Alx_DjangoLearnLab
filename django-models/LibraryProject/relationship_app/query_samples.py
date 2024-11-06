ef5627f1e223d2635131d2dc776a3dcfae253249
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-06 22:41:39 +0300

    add file

---
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -0,0 +1,19 @@
+from relationship_app.models import Author, Book, Library, Librarian
+
+
+def get_books_by_author(author_name):
+    author = Author.objects.get(name=author_name)
+    books = author.books.all()
+    return [book.title for book in books]
+
+
+def get_books_in_library(library_name):
+    library = Library.objects.get(name=library_name)
+    books = library.books.all()
+    return [book.title for book in books]
+
+
+def get_librarian_for_library(library_name):
+    library = Library.objects.get(name=library_name)
+    librarian = library.librarian
+    return librarian.name
