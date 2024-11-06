695bc3221b0a04feb640ce8f3aede60d742e942d
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-07 00:01:05 +0300

    add file

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -40,23 +40,21 @@ class Librarian(models.Model):
 
 class UserProfile(models.Model):
     ROLE_CHOICES = (
-        ("Admin", "Admin"),
-        ("Librarian", "Librarian"),
-        ("Member", "Member"),
+        ('Admin', 'Admin'),
+        ('Librarian', 'Librarian'),
+        ('Member', 'Member'),
     )
-
+    
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default="Member")
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='Member')
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
-
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
-
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
