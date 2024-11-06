bcd657a5c4bd74b4a0ad0df440e00257ce8076d2
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-06 23:56:03 +0300

    add file

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.contrib.auth import login
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponseForbidden
 
 # Create your views here.
 
@@ -39,3 +41,30 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, "register.html", {"form": form})
+
+
+def is_admin(user):
+    return user.userprofile.role == "Admin"
+
+
+def is_librarian(user):
+    return user.userprofile.role == "Librarian"
+
+
+def is_member(user):
+    return user.userprofile.role == "Member"
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, "admin_view.html")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, "librarian_view.html")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, "member_view.html")
