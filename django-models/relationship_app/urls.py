fd8560c03dca603773312cf9bf9600dedf253e50
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-07 19:26:58 +0300

    add file

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -4,9 +4,7 @@ from .views import LibraryDetailView
 from .views import list_books
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from relationship_app.admin_view import admin_view
-from relationship_app.librarian_view import librarian_view
-from relationship_app.member_view import member_view
+
 
 urlpatterns = [
     path("books/", list_books, name="list_books"),
@@ -14,7 +12,4 @@ urlpatterns = [
     path("login/", LoginView.as_view(template_name="login.html"), name="login"),
     path("logout/", LogoutView.as_view(template_name="logout.html"), name="logout"),
     path("register/", views.register, name="register"),
-    path("admin/", admin_view, name="admin_view"),
-    path("librarian/", librarian_view, name="librarian_view"),
-    path("member/", member_view, name="member_view"),
 ]
