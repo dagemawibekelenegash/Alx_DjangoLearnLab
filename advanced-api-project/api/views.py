6541e4b496f640a38208c9a831d8416f70e73b14
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-11-28 23:07:38 +0300

    add file

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,12 +9,36 @@ from rest_framework.generics import (
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 
 class BookListView(ListAPIView):
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [SearchFilter, OrderingFilter]
+    search_fields = ["title", "author__name"]
+    ordering_fields = [
+        "title",
+        "publication_year",
+    ]
+    ordering = ["title"]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        title = self.request.query_params.get("title", None)
+        author = self.request.query_params.get("author", None)
+        publication_year = self.request.query_params.get("publication_year", None)
+
+        if title:
+            queryset = queryset.filter(title__icontains=title)
+        if author:
+            queryset = queryset.filter(author__name__icontains=author)
+        if publication_year:
+            queryset = queryset.filter(publication_year=publication_year)
+
+        return queryset
 
 
 class BookDetailView(RetrieveAPIView):
