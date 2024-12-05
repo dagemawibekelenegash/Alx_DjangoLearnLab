53942da21326ea35885a693783d6d4bc5a026cb5
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-12-05 21:01:53 +0300

    added a file

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,31 @@
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView, UpdateView
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.contrib.auth.models import User
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
-# Create your views here.
+
+class UserLoginView(LoginView):
+    template_name = "blog/login.html"
+
+
+class UserLogoutView(LogoutView):
+    template_name = "blog/logout.html"
+
+
+class UserRegisterView(CreateView):
+    template_name = "blog/register.html"
+    form_class = UserCreationForm
+    success_url = reverse_lazy("login")
+
+
+class UserProfileView(LoginRequiredMixin, UpdateView):
+    template_name = "blog/profile.html"
+    model = User
+    form_class = UserChangeForm
+    success_url = reverse_lazy("profile")
+
+    def get_object(self, queryset=None):
+        return self.request.user
