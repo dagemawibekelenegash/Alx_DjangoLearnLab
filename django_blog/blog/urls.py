df6307ac621bf3941ad5b882606d80e22e22ff68
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-12-05 21:51:24 +0300

    added a file

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,8 +18,8 @@ urlpatterns = [
     path("register/", UserRegisterView.as_view(), name="register"),
     path("profile/", UserProfileView.as_view(), name="profile"),
     path("", PostListView.as_view(), name="post_list"),
-    path("posts/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
-    path("posts/new/", PostCreateView.as_view(), name="post_create"),
-    path("posts/<int:pk>/edit/", PostUpdateView.as_view(), name="post_update"),
-    path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
+    path("post/<int:pk>/", PostDetailView.as_view(), name="post_detail"),
+    path("post/new/", PostCreateView.as_view(), name="post_create"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post_update"),
+    path("post/<int:pk>/delete/", PostDeleteView.as_view(), name="post_delete"),
 ]
