8babb5fe8494600444b542052f6a68e2d1416ffc
dagemawi bekele negash <dagemawibekele11@gmail.com>
2024-12-11 20:24:59 +0300

    added a file

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -3,11 +3,11 @@ from rest_framework.authtoken.models import Token
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import make_password
 
-CustomUser = get_user_model()
+CustomUser = get_user_model().objects.create_user
 
 
 class CustomUserSerializer(serializers.ModelSerializer):
-    token = serializers.CharField(read_only=True)
+    token = serializers.CharField()
 
     class Meta:
         model = CustomUser
@@ -25,12 +25,9 @@ class CustomUserSerializer(serializers.ModelSerializer):
         extra_kwargs = {"password": {"write_only": True}}
 
     def create(self, validated_data):
-        """
-        Create a user and generate a token for the user.
-        """
         validated_data["password"] = make_password(validated_data["password"])
         user = super().create(validated_data)
 
-        token, _ = Token.objects.get_or_create(user=user)
+        token, _ = Token.objects.create(user=user)
         user.token = token.key
         return user
